ef79d5a0bf5ce15090cca8b1862652ecfe1560a1
alialaei <alialaey@gmail.com>
2017-06-04 23:00:43 +0430

    Pulling a web service

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -24,6 +24,49 @@ module.exports = {
       if(err) return next(err);
       if(!customer) return next();
 
+      // var http = require('http');
+      //
+      // function process_response(webservice_response, stock, callback){
+      //   var webservice_data ='';
+      //   webservice_response.on('error', function(e){
+      //     console.log(e.message);
+      //     callback('error in web request for' + stock.symbol);
+      //   });
+      //   webservice_response.on('data', function(chunk){
+      //     webservice_data+= chunk;
+      //   });
+      //   webservice_response.on('end', function(){
+      //     stock_data = JSON.parse(webservice_data);
+      //     stock.current_price = stock_data.LastPrice;
+      //     console.log('stock price' + stock.current_price);
+      //
+      //     //to ensure it has been finished
+      //     callback();
+      //   });
+      // };
+      //
+      // function get_current_price(stock, callback) {
+      //
+      //   options = {
+      //     host: 'dev.markitondemand.com',
+      //     port: 80,
+      //     path: 'MODApis/Api/v2/Quote/JSON?symbol='+ stock.symbol,
+      //     method: 'get'
+      //   };
+      //
+      //   var webservice_request = http.request(options, function (response) {
+      //     process_response(response, stock, callback)
+      //   });
+      //   webservice_request.end();
+      //   console.log(stock.symbol + '-' + stock.current_price);
+      // };
+      //
+      // // this function has been added after install async package for checking is finished last function
+      // async.each(customer.stocks, get_current_price, function(err){
+      //   if(err) console.log(err);
+      //
+      //
+      // });
       res.view({
         customer: customer
       });
