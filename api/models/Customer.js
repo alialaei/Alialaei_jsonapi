ae12ebfc8ca4a409b64aca62194b54cfc29a166a
alialaei <alialaey@gmail.com>
2017-06-04 15:25:20 +0430

    Edit problem of Customer Model

--- api/models/Customer.js
+++ api/models/Customer.js
@@ -5,15 +5,27 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 module.exports = {
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
 
-  attribute: {
+  attributes: {
+    creationDate: {
+      columnName: 'cre_dt',
+      type: 'datetime',
+      defaultsTo: function() {return new Date();}
+    },
+    updateDate: {
+      columnName: 'upd_dt',
+      type: 'datetime',
+      defaultsTo: function() {return new Date();}
+    },
     name: {
       type: 'string',
       required: true
     },
     email:{
       type: 'string',
-      email:true
+      email: true
     },
     state:{
       type:'string'
